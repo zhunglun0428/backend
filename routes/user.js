627dd52b3c80754f4c4bc1e63f6df4b45b74c7fe
grace0950 <pinyan0950@gmail.com>
2023-04-09 14:57:42 +0800

    Feature: add auth in middleware

--- routes/user.js
+++ routes/user.js
@@ -2,10 +2,6 @@ var express = require("express");
 var router = express.Router();
 const userController = require("../controllers/user");
 
-router.get("/", async function (req, res, next) {
-  res.json();
-});
-
 router.post("/register", userController.register);
 router.post("/login", userController.login);
 
