eb1cd9206c0f69b4ed152c9e2d3b27afe721313f
zhunglun0428 <chunlun@MSI.localdomain>
2023-05-15 19:32:16 +0800

    feature: chat history

--- controllers/chat.js
+++ controllers/chat.js
@@ -90,4 +90,20 @@ const getIdleVideo = async (req, res) => {
   }
 };
 
-module.exports = { getImgURL, replyMessage, getIdleVideo };
+const getChatHistory = async (req, res) => {
+  const userId = req.user._id;
+  try {
+    const chat = await Chat.findOne({ userId: userId });
+
+    if (!chat) {
+      res.status(404).json({ message: "Partner not found" });
+    } else {
+      
+      res.status(200).json({ chatHistory: chat.messages });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+module.exports = { getImgURL, replyMessage, getIdleVideo ,getChatHistory};
--- routes/chat.js
+++ routes/chat.js
@@ -7,5 +7,5 @@ const chatController = require("../controllers/chat");
 router.get("/imageURL", auth, chatController.getImgURL);
 router.post("/replyMessage", auth, chatController.replyMessage);
 router.get("/idleVideo", auth, chatController.getIdleVideo);
-
+router.get("/chatHistory", auth, chatController.getChatHistory);
 module.exports = router;
